3a97835e7eadfcf5d91eeadf5bb5c20452783028
Drecrow <155937783+Drecrow@users.noreply.github.com>
2024-12-15 23:58:05 -0500

    Add files via upload

---
+++ CT1/src/CT1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Displaying fictional person details
+    cout << "First Name: Homer" << endl;
+    cout << "Last Name: Simpson" << endl;
+    cout << "Street Address: 742 Evergreen Terrace" << endl;
+    cout << "City: Springfield" << endl;
+    cout << "Zip Code: 49007" << endl;
+
+    return 0;
+}
---
+++ CT1Mod1/src/CT1Mod1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+//Main Function
+int main() {
+//Standard Ouput Statement
+cout << "Welcome to this C++ Program" <<endl;
+cout << "I have corrected all errors for this program." <<endl;
+// Wait For Output Screen
+//Main Function return Statement
+return 0;
+}
